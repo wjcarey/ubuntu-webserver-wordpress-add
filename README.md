1581658052a5f8daa9ebe982d7fd942264a78783
wjcarey <contact@wjcarey.io>
2021-03-04 12:13:28 -0700

    init. upload

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ubuntu-webserver-wordpress-add
-add additional wordpress site to ubuntu webserver
+this script adds additional wordpress site to an ubuntu webserver. the script acts as a wrapper that calls on many smaller scripts to complete the task.
 
-### Run this code below to execute the script to initiate the apache2 create virtualhost configuration.
+### Run this code below to execute the script to initiate the ubuntu-webserver-wordpress-add configuration.
 ~~~
 sudo curl -o ubuntu-webserver-wordpress-add.sh https://raw.githubusercontent.com/wjcarey/ubuntu-webserver-wordpress-add/main/ubuntu-webserver-wordpress-add.sh && sudo chmod 777 ubuntu-webserver-wordpress-add.sh && sudo ./ubuntu-webserver-wordpress-add.sh
 ~~~
--- ubuntu-webserver-wordpress-add.sh
+++ ubuntu-webserver-wordpress-add.sh
@@ -1,1 +1,34 @@
 #!/bin/sh
+
+#DATABASE ARGUMENTS
+echo "\e[32mcreate your database Name ...\e[39m"
+read DATABASE_NAME
+echo "\e[39mdatabase: \e[32m${DATABASE_NAME}\e[39m"
+
+#WORDPRESS ARGUMENTS
+echo "\e[32menter the full path for your wordpress installation ...\e[39m"
+read INSTALL_PATH
+
+#CERTBOT ARGUMENTS
+echo "\e[32mwhat domain name will you use for this wordpress ...\e[39m"
+read DOMAIN_NAME
+
+#DATABASE INSTALL
+echo "downloading webserver software install script from github ..."
+sudo curl -o mysql-database-add.sh https://raw.githubusercontent.com/wjcarey/mysql-database-add/main/mysql-database-add.sh && sudo chmod 777 mysql-database-add.sh && sudo ./mysql-database-add.sh ${DATABASE_NAME}
+
+#MODIFY APACHE
+echo "downloading webserver config script from github ..."
+sudo curl -o apache2-modify-conf.sh https://raw.githubusercontent.com/wjcarey/apache2-modify-conf/master/apache2-modify-conf.sh && sudo chmod 777 apache2-modify-conf.sh && sudo ./apache2-modify-conf.sh
+
+#WORDPRESS INSTALL
+echo "downloading wordpress install script from github ..."
+sudo curl -o wordpress-install.sh https://raw.githubusercontent.com/wjcarey/wordpress-install/master/wordpress-install.sh && sudo chmod 777 wordpress-install.sh && sudo ./wordpress-install.sh ${INSTALL_PATH}
+
+#CERTBOT INSTALL
+echo "downloading certbot install script from github ..."
+sudo curl -o apache2-certbot.sh https://raw.githubusercontent.com/wjcarey/apache2-certbot/main/apache2-certbot.sh && sudo chmod 777 apache2-certbot.sh && sudo ./apache2-certbot.sh ${INSTALL_PATH} ${DOMAIN_NAME}
+
+echo "success: installation complete ..."
+rm -- "$0"
+exit
